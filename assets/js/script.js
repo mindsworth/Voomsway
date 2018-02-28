6d89802fba9fab10308f892b7650e5559e3e6ce6
Chigoziem Nwaiwu <chigodwin20@yahoo.com>
2018-02-28 12:04:55 +0100

    Header Modified

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+ Volume in drive C has no label.
+ Volume Serial Number is A485-D4E7
+
+ Directory of C:\Users\PRINCE CHIGOZIEM\Documents\Practice\exercise\js
+
+02/06/2018  06:57 PM    <DIR>          .
+02/06/2018  06:57 PM    <DIR>          ..
+02/06/2018  06:57 PM                 0 script.js
+               1 File(s)              0 bytes
+               2 Dir(s)  315,118,530,560 bytes free
